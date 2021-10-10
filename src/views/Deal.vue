151e61c22a0a7bc8aa3078446ffb6b3401b53eff
saynaaskh <c001hewanning@qq.com>
2021-10-10 11:33:39 +0800

    2021-10-10: 添加了商品评论，用户可以写评论&删除自己的评论

---
+++ src/views/Deal.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Deal",
+  date() {
+    return {
+      dealId: '',
+      stage: '',
+      productId: '',
+      buyerId: '',
+      sellerId: '',
+      mailingNumber: '',
+      shippingAddress: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
