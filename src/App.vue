e15b7b79bfabb1b7d081fdd4dc3bea4fdfb3ba0e
sdww0 <1315953661@qq.com>
2021-11-24 18:31:21 +0800

    添加花里胡哨的功能

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,27 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    setTimeout(() => {
+      window.L2Dwidget.init({
+        pluginRootPath: 'live2dw/',
+        pluginJsPath: 'lib/',
+        pluginModelPath: 'live2d-widget-model-nico/assets/',
+        tagMode: false,
+        debug: false,
+        model: { jsonPath: '../live2dw/live2d-widget-model-nico/assets/nico.model.json' },
+        display: { position: 'right', width: 200, height: 400 },
+        mobile: { show: true },
+        log: false
+      })
+    }, 1000)
+  },
+}
+
+</script>
+
 <style>
 </style>
